39c6c64ddb8f4db1d0d6eed1b7a69ab282a7dbc7
Daniel Kuehn <daniel@kuehn.se>
2023-07-29 14:23:04 +0200

    Remove if-else that is unnecessary now that _split_nick gracefully handles the case where the nick doesn't have a ! in it

--- ircbot.py
+++ ircbot.py
@@ -153,10 +153,7 @@ class Server(BaseServer):
                         nick, fullname = self._split_nick(line.source)
                         message["nick"] = nick
                 case _:
-                    if '!' in line.source: 
-                        nick, fullname = self._split_nick(line.source)
-                    else: # direct messages don't have "Fully qualified hostnames"
-                        nick = line.source
+                    nick, fullname = self._split_nick(line.source)
                     message["nick"] = nick
 
         constructed_line = None
