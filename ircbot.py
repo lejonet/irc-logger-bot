013e4bd2280bc548fb8d77ae43df7993d2066b71
Daniel Kuehn <daniel@kuehn.se>
2023-08-09 19:03:03 +0200

    Correct variable name

--- ircbot.py
+++ ircbot.py
@@ -187,8 +187,8 @@ class Server(BaseServer):
                 def remove_modes(string):
                     return string.removeprefix("@").removeprefix("+").removeprefix("%").removeprefix("~")
 
-                channel = line.param[2]
-                userlist = map(remove_modes, line.param[3:].split(" "))
+                channel = line.params[2]
+                userlist = map(remove_modes, line.params[3:].split(" "))
                 self.userlists[channel] = set(userlist)
                 self.logger.debug(f"Userlists: {self.userlists}")
             case "PRIVMSG":
